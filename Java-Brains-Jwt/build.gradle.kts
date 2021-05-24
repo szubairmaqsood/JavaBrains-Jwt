36170d3782c18f0d87bdded5d5e1f1ac75f206c1
zubair-pc\zubair <szubair@gmail.com>
2021-05-24 18:14:19 +0500

    Two dependencies are added 1. JJWT 2.Jaxb

--- Java-Brains-Jwt/build.gradle.kts
+++ Java-Brains-Jwt/build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("javax.xml.bind:jaxb-api:2.3.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
